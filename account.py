c30dec3566d622bb06d7eee6dee36a46526da80f
boredsherbet <ccantony064@gmail.com>
2023-04-28 16:20:59 -0500

    updated variable scope, added return statements to remedy logic errors.

--- account.py
+++ account.py
@@ -1,24 +1,26 @@
 class Account:
     def __init__(self, name):
-        self._account_name = name
-        self._account_balance = 0
+        self.__account_name = name
+        self.__account_balance = 0
 
     def deposit(self, amount):
         if amount <= 0:
             return False
         else:
-            self._account_balance += amount
+            self.__account_balance += amount
+            return True
 
     def get_name(self):
-        return self._account_name
+        return self.__account_name
 
     def get_balance(self):
-        return self._account_balance
+        return self.__account_balance
 
     def withdraw(self, amount):
         if amount <= 0:
             return False
-        elif amount >= self._account_balance:
+        elif amount >= self.__account_balance:
             return False
         else:
-            self._account_balance -= amount
+            self.__account_balance -= amount
+            return True
